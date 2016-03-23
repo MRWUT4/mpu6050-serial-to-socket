f8530f2996e20f9ec29a279a95e228c2c16b5caf
David Ochmann <mail@davidochmann.de>
2016-03-23 22:21:15 +0100

    - Added socket.io framework. - Minor refactoring update.

--- js/nodejs/server.js
+++ js/nodejs/server.js
@@ -36,3 +36,29 @@ serial.on( "open", function ()
 });
 
 console.log("\nServer running at http://127.0.0.1:8124/\n");
+
+// var app = require('http').createServer(handler)
+// var io = require('socket.io')(app);
+// var fs = require('fs');
+
+// app.listen(80);
+
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
+
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
--- tools/start-server.command
+++ tools/start-server.command
@@ -2,5 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
-node js/server.js
+node js/nodejs/server.js
 exit 0
